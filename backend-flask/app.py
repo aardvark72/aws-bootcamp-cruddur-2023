4ee784276f8e48e54aff66d292ff14791a0d2044
aardvark72 <jevonrowan1972@gmail.com>
2023-03-06 15:45:02 +0000

    x-ray instrumentation config

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,7 +22,15 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExplorer, SimpleSpanProcessor
 
+# XRAY
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
