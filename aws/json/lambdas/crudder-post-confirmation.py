9b02d6a259657c19e2297e9c1f2bcc03363cf498
aardvark72 <jevonrowan1972@gmail.com>
2023-03-23 08:22:30 +0000

    working on activities

--- aws/json/lambdas/crudder-post-confirmation.py
+++ aws/json/lambdas/crudder-post-confirmation.py
@@ -14,32 +14,26 @@ def lambda_handler(event, context):
     try:
       print('entered-try')
       sql = f"""
-        INSERT INTO public.users (
+         INSERT INTO public.users (
           display_name, 
           email,
           handle, 
           cognito_user_id
           ) 
-        VALUES(
-           '{user_display_name}',
-           '{user_email}',
-           '{user_handle}',
-           '{user_cognito_id}'
-        )  
+        VALUES(%s,%s,%s,%s)
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      cur.execute(sql)
-      conn.commit()
-      #params = [
-      #  user_display_name,
-      #  user_email,
-      #  user_handle,
-      #  user_cognito_id
-      #]
-       
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,*params)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
